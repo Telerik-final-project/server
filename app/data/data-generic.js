1ebd6af744c1c32ad1e507223d42034868f73b78
Targov <alextargov@gmail.com>
2018-05-02 18:24:35 +0300

    Separated the applications routes from the jobs' ones

--- app/data/data-generic.js
+++ app/data/data-generic.js
@@ -1,63 +1,67 @@
 class Data {
-    constructor(Model, includes = []) {
-        this.Model = Model;
-        this.includes = includes;
-    }
+  constructor(Model, includes = []) {
+    this.Model = Model;
+    this.includes = includes;
+  }
 
-    getAll() {
-        return this.Model.findAll({
-            where: {
-                isDeleted: 0,
-            },
-            include: this.includes,
-        });
-    }
+  getAll() {
+    return this.Model.findAll({
+      where: {
+        isDeleted: 0,
+      },
+      include: this.includes,
+    });
+  }
 
-    getById(id) {
-        return this.Model.findById(id, {
-            include: this.includes,
-        });
-    }
+  getById(id) {
+    return this.Model.findById(id, {
+      include: this.includes,
+    });
+  }
 
-    create(obj) {
-        if (obj === null || typeof obj === 'undefined') {
-            throw new Error('Invalid object');
-        }
-        return this.Model.create(obj);
+  create(obj) {
+    if (obj === null || typeof obj === 'undefined') {
+      throw new Error('Invalid object');
     }
+    return this.Model.create(obj);
+  }
 
-    update(id, data) {
-        const tableUpdate = (key, value) => {
-            this.Model.update({
-                [key]: value,
-            }, {
-                    where: {
-                        id: id,
-                    },
-                }, ).success(() => { }).error(() => {
-                    console.log('Invalid tokens!');
-                });
-        };
+  update(id, data) {
+    const tableUpdate = (key, value) => {
+      this.Model.update(
+        {
+          [key]: value,
+        },
+        {
+          where: {
+            id: id,
+          },
+        },
+      );
+    };
 
-        Object.keys(data).forEach((key) => {
-            tableUpdate(key, data[key]);
-        });
-        // data.forEach((dataRowToUpdate) => {
-        //     const attr = Object.keys(dataRowToUpdate)[0];
-        //     const value = dataRowToUpdate[attr];
-        //     tableUpdate(attr, value);
-        // });
-    }
+    Object.keys(data).forEach((key) => {
+      tableUpdate(key, data[key]);
+    });
+    // data.forEach((dataRowToUpdate) => {
+    //     const attr = Object.keys(dataRowToUpdate)[0];
+    //     const value = dataRowToUpdate[attr];
+    //     tableUpdate(attr, value);
+    // });
+  }
 
-    delete(id) {
-        return this.Model.update({
-            isDeleted: 1,
-        }, {
-                where: {
-                    id,
-                },
-            });
-    }
+  delete(id) {
+    return this.Model.update(
+      {
+        isDeleted: 1,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+  }
 }
 
 module.exports = Data;
